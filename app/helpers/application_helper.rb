98a0700e19d825f0a7fa77047bf27c23dc36430d
Lawrance Koo <lawrance.koo@gmail.com>
2014-05-01 23:35:56 +1000

    add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+	#module to provide title on per-page basis 
+	def full_title (page_title)
+		base_title = "Sample App"
+		if page_title.empty?
+			base_title 
+		else
+			base_title + ' | ' + page_title
+		end
+	end 
 end
