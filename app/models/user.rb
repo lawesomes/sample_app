37ed8119bcae1787987becfc9d26f79891230590
Lawrance Koo <lawrance.koo@gmail.com>
2014-05-04 13:44:27 +1000

    Make basic user model and passwords

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+	
+	before_save { self.email = email.downcase }
+	#call back - to save ensure email is saved as lowercase
+
+
+	validates :name, presence: true, length: {maximum: 50}
+	VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+		#/ start of regex
+		#\a match staart of string 
+		#[\w+\-.]+ - at least one word character, plus, hypen or a dot
+		#@ literal 
+		#[a=z\d\-.]+ - at least one letter, digit, hyphen or do 
+		#\. literal dot
+		#[a-z]+ at least one letter 
+		#\z - match end of string 
+		#/end og regex
+		#i case insensitive
+
+	validates :email, presence: true, format: {with: VALIDATE_EMAIL_REGEX}, uniqueness: {case_sensitive: false} 
+	#also need to enforce uniqueness at database level 
+
+	has_secure_password
+	#adds the password / password_confirmation attrbiutes required to compared hashed user input to password_Digest
+	
+	validates :password, length {minimum: 6}
+
+end
